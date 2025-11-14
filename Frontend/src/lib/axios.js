10791fea778135266be03e49203843753827d277
Harshit <hs786soni@gmail.com>
2025-11-14 14:00:50 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: "https://chatapp-2syn.onrender.com",
-  baseURL: "http://localhost:5000/api",
+    baseURL: "https://chatapp-2syn.onrender.com",
+  // baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });

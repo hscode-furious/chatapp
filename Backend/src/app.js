cc1ec223201baa2b5e7f3856c359da7b0e766309
Harshit <hs786soni@gmail.com>
2025-11-14 11:38:49 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://chatapp-2syn.onrender.com",
     credentials: true,
   })
 );
